e6323512f2f2945c0d9f4f8a9eb5a918a2f5b124
lth <lth@litonghui>
2018-07-30 15:53:00 +0800

    删除无用的引用

--- src/main/java/cn/admin/login/controller/InitControl.java
+++ src/main/java/cn/admin/login/controller/InitControl.java
@@ -4,21 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.constraints.Null;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import cn.admin.login.service.InitService;
 import cn.util.TreeNode;
-
 import com.alibaba.druid.util.StringUtils;
 
 
